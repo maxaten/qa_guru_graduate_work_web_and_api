b997a160e0b0909fdccf508f1649974aeb3ac854
Maksim Atenyaev <atenjaev@gmail.com>
2023-07-27 22:16:21 +0600

    commit

---
+++ tests/test_reqres_api.py
@@ -0,0 +1,166 @@
+import allure
+from jsonschema.validators import validate
+
+from utils.helper import load_json_schema
+
+
+class TestReqresApi:
+
+    @allure.tag('api')
+    @allure.title('Создание пользователя')
+    def test_create_user(self, reqres):
+        name = 'Olga'
+        job = 'Manager'
+        schema = load_json_schema('post_create_user.json')
+
+        response = reqres.post(
+            url='/api/users',
+            json={
+                'name': name,
+                'job': job
+        })
+
+        assert response.status_code == 201
+        validate(instance=response.json(), schema=schema)
+        assert response.json()["name"] == name
+        assert response.json()["job"] == job
+
+    @allure.tag('api')
+    @allure.title('Обновление данных пользователя')
+    def test_update_user(self, reqres):
+        name = 'Ololosha'
+        job = 'President'
+        schema = load_json_schema('put_update_user.json')
+
+        response = reqres.put(
+            url='/api/users/2',
+            json={
+                "name": name,
+                "job": job
+            }
+        )
+
+        assert response.status_code == 200
+        validate(instance=response.json(), schema=schema)
+        assert response.json()["name"] == name
+        assert response.json()["job"] == job
+
+    @allure.tag('api')
+    @allure.title('Регистрация пользователя')
+    def test_register_user(self, reqres):
+        email = 'eve.holt@reqres.in'
+        password = 'pistol'
+        schema = load_json_schema('post_register_user.json')
+
+        response = reqres.post(
+            url='/api/register',
+            json={
+                "email": email,
+                "password": password
+            }
+        )
+
+        validate(instance=response.json() , schema=schema)
+        assert response.status_code == 200
+
+
+    @allure.tag('api')
+    @allure.title('Удаление пользователя')
+    def test_delete_user(self, reqres):
+        delete_user = reqres.delete("/api/users/2")
+
+        assert delete_user.status_code == 204
+
+    @allure.tag('api')
+    @allure.title('Обновление некоторых данных клиента')
+    def test_patch_user(self , reqres):
+        name = 'morpheus'
+        job = 'zion resident'
+        schema = load_json_schema('patch_user.json')
+
+        response = reqres.patch(
+            url='/api/users/2',
+            json={
+                "name": name,
+                "job": job
+            }
+        )
+
+        validate(response.json() , schema=schema)
+        assert response.status_code == 200
+        assert response.json()['name'] == name
+        assert response.json()['job'] == job
+
+
+    @allure.tag('api')
+    @allure.title('Загрузка страницы пользователей с задержкой')
+    def test_delayed_response(self, reqres):
+        delay = 3
+        per_page = 6
+        page = 1
+        schema = load_json_schema('get_delay_response.json')
+
+        response = reqres.get(
+            url='/api/users',
+            params={'delay': delay}
+        )
+
+        validate(response.json() , schema=schema)
+        assert response.status_code == 200
+        assert response.json()['per_page'] == per_page
+        assert response.json()['page'] == page
+
+    @allure.tag('api')
+    @allure.title('Успешная авторизация пользователя')
+    def test_login_user(self , reqres):
+        email = 'eve.holt@reqres.in'
+        password = 'cityslicka'
+        schema = load_json_schema('post_login_user.json')
+
+        response = reqres.post(
+            url='/api/login',
+            json={
+                "email": email,
+                "password": password
+            }
+        )
+
+        validate(response.json() , schema=schema)
+        assert response.status_code == 200
+
+    @allure.tag('api')
+    @allure.title('Неуспешная авторизация пользователя')
+    def test_user_login_unsuccessful(self , reqres):
+        email = 'peter@klaven'
+        text_error = 'Missing password'
+        schema = load_json_schema('post_user_login_unsuccessful.json')
+
+        response = reqres.post(
+            url='/api/login',
+            json={
+                'email': email
+            }
+        )
+
+        validate(response.json() , schema=schema)
+        assert response.status_code == 400
+        assert response.json()['error'] == text_error
+
+
+    @allure.tag('api')
+    @allure.title('Неуспешная регистрация пользователя')
+    def test_register_unsuccessful_user(self, reqres):
+        email = 'sydney@fife'
+        text_error = 'Missing password'
+        schema = load_json_schema('post_unregister_user.json')
+
+        response = reqres.post(
+            url='/api/register',
+            json={
+                "email": email
+            }
+        )
+
+        validate(response.json(), schema=schema)
+        assert response.status_code == 400
+        assert response.json()['error'] == text_error
---
+++ utils/helper.py
@@ -0,0 +1,55 @@
+import allure
+from faker import Faker
+import json
+import os.path
+from requests import Session
+from curlify import to_curl
+import logging
+from datetime import datetime
+
+from utils import attach
+
+
+def generate_first_name():
+    fake = Faker(["ru_RU"])
+    return fake.first_name()
+
+
+def generate_last_name():
+    fake = Faker(["ru_RU"])
+    return fake.last_name()
+
+
+def generate_email():
+    fake = Faker()
+    return fake.ascii_free_email()
+
+
+def load_json_schema(file_name: str):
+    path_schema = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../schemas', file_name)
+    with open(path_schema) as schema_json:
+        return json.loads(schema_json.read())
+
+
+class CustomSession(Session):
+    def __init__(self, base_url):
+        self.base_url = base_url
+        super().__init__()
+
+    def request(self, method, url, *args, **kwargs):
+        response = super(CustomSession, self).request(method=method, url=self.base_url + url, *args, **kwargs)
+
+        msg = to_curl(response.request)
+        status_code = response.status_code
+        logging.info(f'{datetime.now()}\nstatus_code={status_code}\n{msg}')
+        with allure.step(f'{method} {url}'):
+            attach.add_request(status_code, msg)
+
+            try:
+                response_body = response.json()
+            except json.JSONDecodeError:
+                response_body = response.text
+
+            attach.add_response(response_body)
+
+        return response
